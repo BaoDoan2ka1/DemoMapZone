2e1af69128cade4afafa1cfed7510fd79bb038e3
Doan Ngoc Quoc Bao <baodnq@vietmap.vn>
2023-11-30 14:24:28 +0700

    Test refactor url

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -1,12 +1,13 @@
 "use client"
+import axios from 'axios'
 
-import React, {useEffect, useRef} from 'react';
-import maplibregl from 'maplibre-gl';
-import 'maplibre-gl/dist/maplibre-gl.css';
-import "./index.scss"
-
+import React, { useEffect, useRef } from 'react';
 import MaplibreGeocoder from '@maplibre/maplibre-gl-geocoder';
 import '@maplibre/maplibre-gl-geocoder/dist/maplibre-gl-geocoder.css';
+
+import * as maplibregl from '../../libs/maplibre-gl.js'
+import '../../libs/maplibre-gl.css'
+import "./index.scss"
 
 export interface ResultSearch {
     ref_id: string
@@ -27,6 +28,20 @@ export interface Boundary {
     full_name: string
 }
 
+export interface _Result {
+    display: string
+    name: string
+    hs_num: string
+    street: string
+    address: string
+    city_id: string
+    city: string
+    district_id: string
+    district: string
+    lat: number
+    lng: number
+}
+
 export default function Map() {
     const mapContainer = useRef<any>(null);
 
@@ -34,70 +49,59 @@ export default function Map() {
         const map = new maplibregl.Map({
             container: 'map',
             style:
-                'http://192.168.10.91:8085/styles/Taiwan-Tile/style.json',
+                'https://tw.map.zone/mt-tw/styles/street/style.json',
             center: [120.896174, 23.681396],
             zoom: 8
         });
 
-        map.addControl(new maplibregl.NavigationControl());
+        map.addControl(new maplibregl.NavigationControl(), 'bottom-right');
 
         const geocoderApi = {
             forwardGeocode: async (config: any) => {
-                const features = [];
+                const features: any[] = [];
                 try {
-                    const request =
-                        `http://192.168.10.111:5256/api/geocodings/search?text=${config.query}`;
+                    const response = await fetch(`https://tw.map.zone/api/search?text=${config.query}`, {
+                        method: "GET",
+                        headers: {
+                            "Content-Type": "application/json"
+                        }
+                    });
 
-                    const response = await fetch(request);
                     const results = await response.json();
+                    for (let i = 0; i < results.length; i++) {
+                        const _temp: _Result = await (await fetch(`https://tw.map.zone/api/place?refid=${results[i].ref_id}`)).json()
 
-                    const listApi = results.map((item: ResultSearch) => {
-                        return `http://192.168.10.111:5256/api/geocodings/place?refid=${item.ref_id}`
-                    })
+                        const point = {
+                            type: 'Feature',
+                            geometry: {
+                                type: 'Point',
+                                coordinates: [_temp.lng, _temp.lat]
+                            },
+                            place_name: _temp.display || _temp.name,
+                            text: _temp.display || _temp.name,
+                            place_type: ['place'],
+                            center: [_temp.lng, _temp.lat]
+                        };
 
-                    Promise.all(listApi).then((values) => {
-                        console.log(values)
-                    })
-
-                    // for (const feature of geojson.features) {
-                    //     const center = [
-                    //         feature.bbox[0] +
-                    //         (feature.bbox[2] - feature.bbox[0]) / 2,
-                    //         feature.bbox[1] +
-                    //         (feature.bbox[3] - feature.bbox[1]) / 2
-                    //     ];
-                    //     const point = {
-                    //         type: 'Feature',
-                    //         geometry: {
-                    //             type: 'Point',
-                    //             coordinates: center
-                    //         },
-                    //         place_name: feature.properties.display_name,
-                    //         properties: feature.properties,
-                    //         text: feature.properties.display_name,
-                    //         place_type: ['place'],
-                    //         center
-                    //     };
-                    //     features.push(point);
-                    // }
+                        features.push(point);
+                    }
                 } catch (e) {
                     console.error(`Failed to forwardGeocode with error: ${e}`);
                 }
 
                 return {
-                    // features
+                    features
                 };
             }
         };
 
         map.addControl(
-            new MaplibreGeocoder(geocoderApi, {
-                maplibregl
-            })
+            new MaplibreGeocoder(geocoderApi, { maplibregl }),
+            "top-left"
         );
     }, []);
 
     return <div className="map-wrap">
-        <div id="map" ref={mapContainer} className="map"/>
+        <div id="map" ref={mapContainer} className="map" />
     </div>
 }
